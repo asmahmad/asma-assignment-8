683360379ec8128b2b38d84a1587c41d61735083
asmahmad <121257988+asmahmad@users.noreply.github.com>
2023-02-21 14:06:11 -0800

    Assignment8 v.2

--- test/com/coderscampus/assignment8/Assignment8Test.java
+++ test/com/coderscampus/assignment8/Assignment8Test.java
@@ -15,41 +15,41 @@ import org.junit.jupiter.api.Test;
 
 class Assignment8Test {
 	
-    @Test
-    
-    public void getData () {
-  
-        Assignment8 assignment = new Assignment8();
-        List<Integer> numbersList = new ArrayList<>();
-        long startTime = System.currentTimeMillis();
-       
-        for (int i=0; i<1000; i++) {
-
-           numbersList = assignment.getNumbers();
-
-            System.out.println(numbersList);
-
-        }
-        // Tried to implement synchronous method to count frequencies, not the scope of this assignment. 
-        // But was able to do it. If you have time would you please take a look and just explain where I am going wrong.
-        //Thank you :)
-
-    	 Map<Integer, Integer> frequencyMap = new HashMap<>();
-    	 
-         for (Integer n: numbersList)
-         {
-        	 frequencyMap.merge(n, 1, Integer::sum);
-         }
-  
-         for (Entry<Integer, Integer> entry: frequencyMap.entrySet()) {
-             System.out.println(entry.getKey() + " =  " + entry.getValue());
-         }
-        
-        long estimatedTimeMilliSeconds = System.currentTimeMillis() - startTime;
-        long estimatedTimeMinutes =  (estimatedTimeMilliSeconds/1000)/60;
-        System.out.println("Time taken to read data from file:	"+ estimatedTimeMinutes +" minutes");
-        
-    }
+//    @Test
+//    
+//    public void getData () {
+//  
+//        Assignment8 assignment = new Assignment8();
+//        List<Integer> numbersList = new ArrayList<>();
+//        long startTime = System.currentTimeMillis();
+//       
+//        for (int i=0; i<1000; i++) {
+//
+//           numbersList = assignment.getNumbers();
+//
+//            System.out.println(numbersList);
+//
+//        }
+//        // Tried to implement synchronous method to count frequencies, not the scope of this assignment. 
+//        // But was able to do it. If you have time would you please take a look and just explain where I am going wrong.
+//        //Thank you :)
+//
+//    	 Map<Integer, Integer> frequencyMap = new HashMap<>();
+//    	 
+//         for (Integer n: numbersList)
+//         {
+//        	 frequencyMap.merge(n, 1, Integer::sum);
+//         }
+//  
+//         for (Entry<Integer, Integer> entry: frequencyMap.entrySet()) {
+//             System.out.println(entry.getKey() + " =  " + entry.getValue());
+//         }
+//        
+//        long estimatedTimeMilliSeconds = System.currentTimeMillis() - startTime;
+//        long estimatedTimeMinutes =  (estimatedTimeMilliSeconds/1000)/60;
+//        System.out.println("Time taken to read data from file:	"+ estimatedTimeMinutes +" minutes");
+//        
+//    }
     
     @Test
     
@@ -70,14 +70,12 @@ class Assignment8Test {
    	while (tasks.stream().filter(CompletableFuture::isDone).count()<1000) {}
     	 
     	ConcurrentMap<Integer, Integer> numberFrequencyMap = new ConcurrentHashMap<>();
-
         for (CompletableFuture<List<Integer>> numbers : tasks) {
             try {
                 List<Integer> number = numbers.get();
                 	for(int n :number) {
                 		 synchronized (numberFrequencyMap) {
-                        	 numberFrequencyMap.merge(n, 1, Integer::sum);
-                        	
+                        	 numberFrequencyMap.merge(n, 1, Integer::sum); 	
         				}   
                 	}
             } catch (InterruptedException | ExecutionException e) {
@@ -91,7 +89,9 @@ class Assignment8Test {
     	
         long estimatedTimeMilliSeconds = System.currentTimeMillis() - startTime;
         long estimatedTimeMinutes =  (estimatedTimeMilliSeconds/1000)/60;
-        System.out.println("Time taken to read data from file:	"+ estimatedTimeMinutes +" minutes");
+        System.out.println("Time taken to read data from file:	"+ estimatedTimeMilliSeconds +" millisec");
+        System.out.println("Time taken to read data from file:	"+ estimatedTimeMinutes +" Minutes");
+
     	
     }
     
